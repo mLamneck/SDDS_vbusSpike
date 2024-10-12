2200fd66a242c54c223f1739dd5261b5b9b6b282
Mark Lamneck <mark.lamneck@mpci.de>
2024-10-12 10:24:08 +0200

    some changes in dummy project

--- main.h
+++ main.h
@@ -14,21 +14,46 @@ class TsubMenu : public TmenuHandle{
 		)
 		public:
 			TsubMenu(){
-				
+			
 			}
 };
 
 class TuserStruct : public TmenuHandle{
     Ttimer timer;
+    Ttimer timer2;
+	int status = 0;
     public:
         sdds_struct(
 			sdds_var(Tuint8,val1)
+			sdds_var(Tuint8,val2)
+			sdds_var(Tuint8,val3)
+			sdds_var(Tuint8,val4)
 			sdds_var(TsubMenu,sub)
         )
         public:
             TuserStruct(){
                 simUart1.FlinkedUart = &simUart2; 
                 simUart2.FlinkedUart = &simUart1;    
+
+				timer.start(1000);
+				on(timer){
+					switch(status){
+						case 0 :
+							val1++;
+							status=1;
+							break; 
+						case 1 : 
+							val2++;
+							val3++;
+							status=0;
+							break; 
+						case 2 : 
+							break;
+						
+					}
+					timer.start(1000);
+				};
+
             }
 } userStruct;
 
