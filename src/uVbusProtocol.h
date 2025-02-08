89e42538c12e69239e0c75d8ea90e9583cafb095
Mark Lamneck <mark.lamneck@mpci.de>
2025-02-08 15:27:48 +0100

    TbufferStream moved to uMemoryUtils of sdds

--- src/uVbusProtocol.h
+++ src/uVbusProtocol.h
@@ -2,6 +2,7 @@
 #define UVBUSPROTOCOL_H
 
 #include "uPlatform.h"
+#include <uMemoryUtils.h>
 
 class TvbusProtocoll{
 	public:
@@ -87,168 +88,13 @@ class TvbusProtocoll{
 */
 };
 
-class TbufferStream{
-	typedef dtypes::uint8 uint8;
-	protected:
-		dtypes::uint8* Fbuffer = nullptr;
-		int FbufferSize = 0;
-		int FbytesAvailableForRead = 0;
-		int FreadPos = 0;
-		int FwritePos = 0;
-	public:
-		constexpr dtypes::uint8* buffer() { return Fbuffer; }
-
-		int writePos() { return FwritePos; }
-		void setWritePos(int _val) { FwritePos = _val; }
-		int readPos() { return FreadPos; }
-		void setReadPos(int _val) { FreadPos = _val; }
-
-		void init(dtypes::uint8* _buffer, int _size, int _availableForRead = 0){
-			Fbuffer = static_cast<uint8*>(_buffer);
-			FbufferSize = _size;
-			FbytesAvailableForRead = _availableForRead;
-			FreadPos = 0;
-			FwritePos = 0;
-		}
-
-		void move(int _nBytes){
-			memcpy(&Fbuffer[FwritePos], &Fbuffer[FreadPos], _nBytes);
-			FreadPos+=_nBytes;
-			FwritePos+=_nBytes;
-		}
-
-	 	int bytesAvailableForRead() { return FbytesAvailableForRead - FreadPos; }
-		
-		bool hasSpaceFor(const uint8 _size){
-			return ((FwritePos + _size) < FbufferSize);
-		}
-
-		int spaceAvailableForWrite() { return FbufferSize - FwritePos; }
-		
-		bool writeByte(const uint8 _byte){ return writeVal(_byte); }
-		bool writeWord(const dtypes::uint16 _val){ return writeVal(_val); }
-		
-		void writeBytesNoCheck(const void* _ptr, int _n){
-			memcpy(&Fbuffer[FwritePos], _ptr, _n);
-			FwritePos += _n;
-		}
-
-		int writeBytes(const void* _ptr, int _n, bool _skipIfdoesntFit = false){
-			auto spaceAvailable = spaceAvailableForWrite();
-			if (_n > spaceAvailable){
-				if (_skipIfdoesntFit) return 0;
-				_n = spaceAvailable;
-			} 
-			memcpy(&Fbuffer[FwritePos], _ptr, _n);
-			FwritePos += _n;
-			return _n;
-		}
-
-		int readBytes(void* _dst, int _n){
-			if (bytesAvailableForRead() < _n) return 0;
-			memcpy(_dst, &Fbuffer[FreadPos], _n);
-			FreadPos += _n;
-			return _n;
-		}
-
-		template <typename T>
-		bool writeVal(const T _value){
-			if (!hasSpaceFor(sizeof(T))) return false;
-			memcpy(&Fbuffer[FwritePos], &_value, sizeof(T));
-			FwritePos += sizeof(T);
-			return true;
-		}
-
-		template <typename T>
-		bool readVal(T& _value){
-			if (bytesAvailableForRead() < (int)sizeof(T)) return false;
-			memcpy(&_value,&Fbuffer[FreadPos], sizeof(T));
-			FreadPos += sizeof(T);
-			return true;
-		}
-
-		/* this can be used to load data from complex objects. This will be usefull if we use the protocoll
-			on udp where Taddr contains IP and Port. It might be event useful now to declare Taddr as class
-			and have methods like isBroadcast on it. But I have no time to test this now
-		template <typename T>
-		typename std::enable_if<std::is_trivially_copyable<T>::value && !std::is_class<T>::value && sizeof(T) == 1, bool>::type
-		readVal(T& _value){
-			if (bytesAvailableForRead() < (int)sizeof(T)) return false;
-			_value = Fbuffer[FreadPos];
-			FreadPos += sizeof(T);
-			return true;
-		}
-		
-		template <typename T>
-		typename std::enable_if<std::is_trivially_copyable<T>::value && !std::is_class<T>::value && sizeof(T) != 1, bool>::type
-		readVal(T& _value){
-			if (bytesAvailableForRead() < (int)sizeof(T)) return false;
-			memcpy(&_value,&Fbuffer[FreadPos], sizeof(T));
-			FreadPos += sizeof(T);
-			return true;
-		}
-
-		template <typename T>
-		typename std::enable_if<std::is_class<T>::value, bool>::type
-		readVal(T& _value) {
-			//toBe done ....
-			//if (!_value.deserialize(buffer + readPos, size - readPos)) return false;
-			//readPos += sizeof(T);
-			//static_assert(false,"not implemented");
-			static_assert(!std::is_class<T>::value,"not implemented");
-			return false;
-		}
-
-		*/
-
-		bool readOfs(dtypes::uint8 _ofs){
-			if (_ofs >= bytesAvailableForRead()) return false;
-			FreadPos += _ofs;
-			return true;
-		}
-
-		template <typename T, int OFS>
-		void writeValToOfs(const T _val){
-			memcpy(&Fbuffer[OFS], &_val, sizeof(T));
-		};
-
-		template <int OFS>
-		constexpr void writeValToOfs(const dtypes::uint8 _val){
-			Fbuffer[OFS]=_val;
-		}
-
-		template <typename T>		
-		void writeValToOfs(int _ofs, const T _val){
-			memcpy(&Fbuffer[_ofs], &_val, sizeof(T));
-		}
-
-		void writeValToOfs(int _ofs, const dtypes::uint8 _val){
-			Fbuffer[_ofs] =  _val;
-		}
-
-		void writeByteToOfs(int _ofs, const dtypes::uint8 _val){
-			Fbuffer[_ofs] =  _val;
-		}
-
-		template <typename T, int OFS>
-		T readValFromOfs() {
-			T value;
-			memcpy(&value, &Fbuffer[OFS], sizeof(T));
-			return value;
-		}
-
-		template <int OFS>
-		constexpr dtypes::uint8 readValFromOfs() {
-			return Fbuffer[OFS];
-		}
-};
 
 template <
     typename _t_prot_addr,
     typename _t_prot_port,
 	int max_msg_length
 >
-class TvbusProtStream : public TbufferStream{
+class TvbusProtStream : public sdds::memUtils::TbufferStream{
 	public:
 		typedef TvbusProtStream<
 			_t_prot_addr,
@@ -302,7 +148,7 @@ class TvbusProtStream : public TbufferStream{
 		constexpr void setSendPending(){ FsendPending = true; }		
 
 		constexpr void init(uint8* _buffer){
-			TbufferStream::init(_buffer,c_max_msg_length);
+			sdds::memUtils::TbufferStream::init(_buffer,c_max_msg_length);
 			Fbuffer = static_cast<uint8*>(_buffer);
 			FreadPos = HEADER_SIZE;
 			FwritePos = HEADER_SIZE;
